1e96fc7091de71f71886944a593e669a19adde79
b4k3m0n00 <catking267@gmail.com>
2024-11-07 22:25:43 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/1.h
@@ -0,0 +1,2 @@
+float far = 9.0 / 5.0;
+float heit = 32;
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "1.h"
+
+using namespace std;
+
+float celsius;
+
+extern float far;
+extern float heit;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+
+    static float fahrenheit;
+
+    cout << "Введите температуру в градусах Цельсия: ";
+    cin >> celsius;
+
+    register auto tempfahrenheit = celsius * far + heit;
+
+    fahrenheit = tempfahrenheit;
+
+    cout << "Температура в градусах Фаренгейта: " << fahrenheit << endl;
+
+    return 0;
+
+}
